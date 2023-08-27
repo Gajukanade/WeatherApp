d081085080ff08b2f66571dc4f7b62ca45de5216
GajuKanade <73280285+Gajukanade@users.noreply.github.com>
2023-08-27 17:21:26 +0530

    commit1

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "00ad94a2f3de8b7efcb154a7214ccf5a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
